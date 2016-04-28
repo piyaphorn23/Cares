5293901fb305e80d21b923524c803d6f5fbaf7c6
piyaphorn schueller <550107030043@dpu.ac.th>
2016-04-28 16:59:34 +0700

    create map with Mapkit

---
+++ Cares/MapkitViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MapkitViewController.swift
+//  Cares
+//
+//  Created by DE DPU on 4/28/2559 BE.
+//  Copyright © 2559 MyApplication. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapkitViewController: UIViewController {
+
+    
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Cares/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cares
+//
+//  Created by DE DPU on 4/27/2559 BE.
+//  Copyright © 2559 MyApplication. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
