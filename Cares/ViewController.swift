5adfcb0fd24b2780fad2242b9ed5dfe8c5471dfd
piyaphorn schueller <550107030043@dpu.ac.th>
2016-04-29 14:07:39 +0700

    Kumulos in form sign up insert success!!

--- Cares/ViewController.swift
+++ Cares/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtConfirmPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonSignUp_OnClick() {
+        if txtPassword.text == txtConfirmPassword.text {
+            let kumuAPI = Kumulos()
+            kumuAPI.createUserWithUsername(txtUsername.text, andPassword: txtPassword.text, andEmail: txtEmail.text, andAuthData: "test")
+        }
+    }
 
+    @IBAction func buttonCancel_OnClick() {
+        txtUsername.text = ""
+        txtPassword.text = ""
+        txtEmail.text = ""
+        txtConfirmPassword.text = ""
+    }
 }
 
