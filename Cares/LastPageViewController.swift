62d6020cb7b06809928711fdacbb3f9a589c560c
piyaphorn schueller <550107030043@dpu.ac.th>
2016-05-16 13:47:20 +0700

    Update Login/Logout with Facebook

---
+++ Cares/LastPageViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LastPageViewController.swift
+//  Cares
+//
+//  Created by DE DPU on 5/16/2559 BE.
+//  Copyright © 2559 MyApplication. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+
+class LastPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Cares/LoginViewController.swift
+++ Cares/LoginViewController.swift
@@ -10,13 +10,33 @@ import UIKit
 import FBSDKCoreKit
 import FBSDKLoginKit
 
-class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
+class LoginViewController: UIViewController, FBSDKLoginButtonDelegate
+{
 
+    @IBOutlet weak var lblUsername: UITextField!
+    @IBOutlet weak var lblPassword: UITextField!
+    @IBOutlet weak var btnFacebook: FBSDKLoginButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        if (FBSDKAccessToken.currentAccessToken() == nil)
+        {
+            print("Not logged in...")
+        }
+        else
+        {
+            print("Logged in...")
+        }
         
-        // Do any additional setup after loading the view.
+        var loginButton = FBSDKLoginButton()
+        loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+        loginButton.center = self.view.center
+        
+        loginButton.delegate = self
+        
+        self.view.addSubview(loginButton)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,15 +44,43 @@ class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    /*func configureFacebook()
+    {
+        btnFacebook.readPermissions = ["public_profile", "email", "user_friends"];
+        btnFacebook.delegate = self
+    }*/
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!)
+    {
+        
+        if error == nil{
+            print("Login complete.")
+            self.performSegueWithIdentifier("showNew", sender: self)
+        }
+        else{
+            print(error.localizedDescription)
+        }
+        
+        //FBSDKGraphRequest.init(graphPath: "me", parameters: ["fields":"first_name, last_name, picture.type(large)"]).startWithCompletionHandler { (connection, result, error) -> Void in
+            
+            /*let strFirstName: String = (result.objectForKey("first_name") as? String)!
+            let strLastName: String = (result.objectForKey("last_name") as? String)!
+            let strPictureURL: String = (result.objectForKey("picture")?.objectForKey("data")?.objectForKey("url") as? String)!*/
+            
+            /*self.lblName.text = "Welcome, \(strFirstName) \(strLastName)"
+            self.ivUserProfileImage.image = UIImage(data: NSData(contentsOfURL: NSURL(string: strPictureURL)!)!)*/
     }
-    */
-
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!)
+    {
+        let loginManager: FBSDKLoginManager = FBSDKLoginManager()
+        loginManager.logOut()
+        
+        /*ivUserProfileImage.image = nil
+        lblName.text = ""*/
+    }
+    
 }
+
+
+
